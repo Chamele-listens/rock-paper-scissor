f39e1c1e784f37233ae2a5cdcaadc8e7d05b11f5
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-05 23:24:11 +0700

    Add playRound function

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let computerChoice = ["rock","paper","scissor"]
+let computerPick;
 
 function getComputerChoice(){
-    let computerPick = computerChoice[Math.floor(Math.random() * computerChoice.length)]
+    computerPick = computerChoice[Math.floor(Math.random() * computerChoice.length)]
     return computerPick
 }
 
-console.log(getComputerChoice());
+let playerSelect = prompt("Pick rock, paper or scissor").toLowerCase();
+let computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+console.log(playerSelect)
+
+function playRound(playerSelect,computerSelection){
+    if (playerSelect == "paper" && computerSelection == "rock"){
+        return "You win ! Paper beats rock "
+    } else if (playerSelect == "rock" && computerSelection == "scissor"){
+        return "You win ! Rock beats scissor"
+    } else if (playerSelect == "scissor" && computerSelection == "paper"){
+        return "You win ! Scissor beats paper"
+    }else if (playerSelect == computerSelection){
+        return "It's a tie !"
+    }
+    else{
+        return `You lose. ${computerSelection} beats ${playerSelect}`
+    }
+}
+
+console.log(playRound(playerSelect,computerSelection))
