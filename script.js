402208436e9281aa96525b8f566a7f5733e6147c
Chamele <114728861+Chamele-listens@users.noreply.github.com>
2024-02-13 21:40:46 +0700

    Add game logic to eventListener and create p element

--- script.js
+++ script.js
@@ -6,12 +6,30 @@ function getComputerChoice(){
     return computerPick
 }
 
-let playerSelect = document.querySelectorAll("button");
+let playerBtn = document.querySelectorAll("button");
 let computerSelection;
+let resultDisplay = document.querySelector("div")
+let winnerDisplay = document.createElement("p")
+resultDisplay.appendChild(winnerDisplay)
 
-playerSelect.forEach((button) => {
-    button.addEventListener("click", () => {
-        alert(button.value);
+playerBtn.forEach((playerOptions) => {
+    playerOptions.addEventListener("click", () => {
+        computerSelection = getComputerChoice();
+        let playerSelect = playerOptions.value
+        resultDisplay.textContent = `player: ${playerSelect}, computer: ${computerSelection}`;
+
+        if (playerSelect == "paper" && computerSelection == "rock"){
+            return "You win ! Paper beats rock "
+        } else if (playerSelect == "rock" && computerSelection == "scissor"){
+            return "You win ! Rock beats scissor"
+        } else if (playerSelect == "scissor" && computerSelection == "paper"){
+            return "You win ! Scissor beats paper"
+        }else if (playerSelect == computerSelection){
+            return "It's a tie !"
+        }
+        else{
+            return `You lose. ${computerSelection} beats ${playerSelect}`
+        }
     })
     
 });
@@ -25,18 +43,7 @@ function playRound(playerSelect,computerSelection){
     console.log(computerSelection);
     console.log(playerSelect)
 
-    if (playerSelect == "paper" && computerSelection == "rock"){
-        return "You win ! Paper beats rock "
-    } else if (playerSelect == "rock" && computerSelection == "scissor"){
-        return "You win ! Rock beats scissor"
-    } else if (playerSelect == "scissor" && computerSelection == "paper"){
-        return "You win ! Scissor beats paper"
-    }else if (playerSelect == computerSelection){
-        return "It's a tie !"
-    }
-    else{
-        return `You lose. ${computerSelection} beats ${playerSelect}`
-    }
+    
 }
 
 
